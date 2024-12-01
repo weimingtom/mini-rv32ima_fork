2f974a91f198ea274c426d1679fee1e91c39a74b
weimingtom <weimingtom@qq.com>
2024-12-02 05:32:21 +0800

    test failed

---
+++ mini-rv32ima/testrv32ima/start_linux-6.1.14.bat
@@ -0,0 +1,6 @@
+::Welcome to Buildroot
+::buildroot login: root
+::~ # halt
+
+mini-rv32ima.exe -f linux-6.1.14-rv32nommu-cnl-1.img
+pause
---
+++ mini-rv32ima/testrv32ima/start_linux-6.8_failed.bat
@@ -0,0 +1,2 @@
+mini-rv32ima.exe -f linux-6.8_rv32nommu_Image.img
+pause
